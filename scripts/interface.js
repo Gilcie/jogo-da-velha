efec279711ec56f97c21e76965d1a47608736d18
Gilcie <gilcieb147@gmail.com>
2023-03-27 10:36:52 -0300

    version1

--- scripts/interface.js
+++ scripts/interface.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     let squares = document.querySelectorAll('.square');
 
     squares.forEach((square) => {
-        square.addEventListener('click', handleClick)
+        square.addEventListener('click', handleClick);
     })
 
 
@@ -11,6 +11,38 @@ document.addEventListener('DOMContentLoaded',()=>{
 
 function handleClick(event){
 
-    console.log(event.target)
+    let square = event.target;
+    let position = square.id
+
+   if(handleMove(position)){
+        
+        setTimeout(() => {
+            alert(`O jogo acabou - O vencedor foi o jogador ${playerTime}`);
+        }, 10);
+        
+   };
+    updateSquare(position);
 
 }
+
+function updateSquare(position){
+    let square = document.getElementById(position.toString());
+    let symbol = board[position];
+    square.innerHTML = `<div class='${symbol}'></div>`
+}
+
+function updateSquares(){
+    let squares = document.querySelectorAll('.square');
+
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+        if(symbol != ''){
+            square.innerHTML = `<div class='${symbol}'></div>`
+        }
+    })
+}
+
+
+
+
